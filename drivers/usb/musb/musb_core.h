skitlab/kernel-source-dm8168-3.2.y

musb: cppi41: adding musb CPPI41 DMA support
Texas Instruments CPPI 4.1 DMA support for the MUSBMHRDC driver.

The code makes use of the accelerated "generic RNDIS" DMA mode, falling back to
the transparent (packet per transfer) mode if the hardware restrictions prevent
it from using the accelerated mode.

Update:
        - has update for is_cppi41_enabled() due to musb changes

Signed-off-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

drivers/usb/musb/musb_core.h
@@ -236,6 +236,7 @@ enum musb_g_ep0_state {
 #define		MUSB_GLUE_DMA_INVENTRA				0x0008
 #define		MUSB_GLUE_DMA_CPPI					0x0010
 #define		MUSB_GLUE_DMA_TUSB					0x0020
+#define		MUSB_GLUE_DMA_CPPI41					0x0040
 
 
 /**
@@ -672,6 +673,8 @@ static inline const char *get_dma_name(struct musb *musb)
 		return "dma-inventra";
 	else if (musb->ops->flags & MUSB_GLUE_DMA_CPPI)
 		return "dma-cppi3";
+	else if (musb->ops->flags & MUSB_GLUE_DMA_CPPI41)
+		return "dma-cppi41";
 	else if (musb->ops->flags & MUSB_GLUE_DMA_TUSB)
 		return "dma-tusb-omap";
 	else

ohporter/linux-am33x

drivers/usb/musb/musb_core.h
@@ -186,6 +186,7 @@ enum musb_g_ep0_state {
 #define		MUSB_GLUE_DMA_CPPI					0x0010
 #define		MUSB_GLUE_DMA_TUSB					0x0020
 #define		MUSB_GLUE_DMA_UX500					0x0040
+#define		MUSB_GLUE_DMA_CPPI41					0x0080
 
 
 /**
@@ -620,6 +621,8 @@ static inline const char *get_dma_name(struct musb *musb)
 		return "dma-inventra";
 	else if (musb->ops->flags & MUSB_GLUE_DMA_CPPI)
 		return "dma-cppi3";
+	else if (musb->ops->flags & MUSB_GLUE_DMA_CPPI41)
+		return "dma-cppi41";
 	else if (musb->ops->flags & MUSB_GLUE_DMA_TUSB)
 		return "dma-tusb-omap";
 	else