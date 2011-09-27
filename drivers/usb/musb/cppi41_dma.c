embest-tech/rowboat-kernel

musb: adding support for am335x
am335x has two musb interface and uses cppi4.1 dma engine. It's
almost same as ti81xx and so using ti81xx files for am335x.

Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

drivers/usb/musb/cppi41_dma.c
@@ -184,7 +184,7 @@ static int __devinit cppi41_controller_start(struct dma_controller *controller)
 	cppi = container_of(controller, struct cppi41, controller);
 	cppi_info = cppi->cppi_info;
 
-	if (cpu_is_ti81xx()) {
+	if (cpu_is_ti81xx() || cpu_is_am335x()) {
 		cppi->automode_reg_offs = TI81XX_USB_AUTOREQ_REG;
 		cppi->teardown_reg_offs = TI81XX_USB_TEARDOWN_REG;
 	} else {
@@ -1418,14 +1418,14 @@ static void usb_process_rx_queue(struct cppi41 *cppi, unsigned index)
 		if (unlikely(rx_ch->channel.actual_len >= rx_ch->length ||
 			     length < orig_buf_len)) {
 
-#ifdef CONFIG_ARCH_TI81XX
+#if defined(CONFIG_ARCH_TI81XX) || defined(CONFIG_ARCH_AM335X)
 			struct musb_hw_ep *ep;
 			u8 isoc, next_seg = 0;
 
 			/* Workaround for early rx completion of
 			 * cppi41 dma in Generic RNDIS mode for ti81xx
 			 */
-			if (cpu_is_ti81xx() && is_host_enabled(cppi->musb)) {
+			if (is_host_enabled(cppi->musb)) {
 				u32 pkt_size = rx_ch->pkt_size;
 				ep = cppi->musb->endpoints + ep_num;
 				isoc = musb_readb(ep->regs, MUSB_RXTYPE);

ohporter/linux-am33x

drivers/usb/musb/cppi41_dma.c
@@ -190,7 +190,7 @@ static int __devinit cppi41_controller_start(struct dma_controller *controller)
 	cppi_info = cppi->cppi_info;
 	musb = cppi->musb;
 
-	if (cpu_is_ti81xx()) {
+	if (cpu_is_ti81xx() || cpu_is_am335x()) {
 		cppi->automode_reg_offs = TI81XX_USB_AUTOREQ_REG;
 		cppi->teardown_reg_offs = TI81XX_USB_TEARDOWN_REG;
 	} else {
@@ -1507,14 +1507,14 @@ static void usb_process_rx_queue(struct cppi41 *cppi, unsigned index)
 		if (unlikely(rx_ch->channel.actual_len >= rx_ch->length ||
 			     length < orig_buf_len)) {
 
-#ifdef CONFIG_SOC_OMAPTI81XX
+#if defined(CONFIG_SOC_OMAPTI81XX) || defined(CONFIG_SOC_OMAPAM33XX)
 			struct musb_hw_ep *ep;
 			u8 isoc, next_seg = 0;
 
 			/* Workaround for early rx completion of
 			 * cppi41 dma in Generic RNDIS mode for ti81xx
 			 */
-			if (cpu_is_ti81xx() && is_host_enabled(cppi->musb)) {
+			if (is_host_enabled(cppi->musb)) {
 				u32 pkt_size = rx_ch->pkt_size;
 				ep = cppi->musb->endpoints + ep_num;
 				isoc = musb_readb(ep->regs, MUSB_RXTYPE);